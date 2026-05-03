5338043de19b4a81b2c7f8c98a91d82aa01db634
Cmorgan124 <cdtm1245@gmail.com>
2026-05-03 17:34:52 -0500

    started working on the outline functions

--- Assets/Code/Scripts/Towers/UpgradeHandler.cs
+++ Assets/Code/Scripts/Towers/UpgradeHandler.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UpgradeHandler : MonoBehaviour
+public class UpgradeHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Material outline;
+    [SerializeField] private Material normal;
+    [SerializeField] private Renderer _renderer;
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Hovering over: " + gameObject.name);
+        _renderer.material = outline;
+    }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _renderer.material = normal;
+    }
 }
