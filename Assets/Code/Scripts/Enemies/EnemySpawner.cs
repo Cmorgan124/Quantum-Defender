61e5d8da6bfa83c6bd0b47a391a8e727210c9ebc
Cmorgan124 <cdtm1245@gmail.com>
2026-07-02 14:18:27 -0500

    Added infrared toggle

--- Assets/Code/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Code/Scripts/Enemies/EnemySpawner.cs
@@ -9,39 +9,78 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] GameObject atomringed;
     [SerializeField] GameObject tank;
     [SerializeField] GameObject tankringed;
-    [SerializeField] GameObject shield;
     [SerializeField] GameObject speedster;
     [SerializeField] GameObject speedsterringed;
-    [SerializeField] GameObject infared;
+    [SerializeField] GameObject shield;
     [SerializeField] GameObject boss;
+
+
+    [Header("Infrared Stuff")]
+    private bool isInfrared = false;
+    [SerializeField] Material infraredMaterial;
+
+    public void Infrared()
+    {
+        isInfrared = !isInfrared;
+    }
 
     public void SpawnAtom()
     {
         GameObject Atom = Instantiate(atom, LevelManager.Instance.startPoint.position, Quaternion.identity); 
+        if (isInfrared)
+        {
+          Renderer _Renderer = Atom.GetComponent<Renderer>();
+          _Renderer.material = new Material(infraredMaterial);
+        }
     }
     public void SpawnAtomRinged()
     {
         GameObject Atomringed = Instantiate(atomringed, LevelManager.Instance.startPoint.position, Quaternion.identity); 
+        if (isInfrared)
+        {
+          Renderer _Renderer = Atomringed.GetComponent<Renderer>();
+          _Renderer.material = new Material(infraredMaterial);
+        }
     }
 
     public void SpawnTank()
     {
-        GameObject Tank = Instantiate(tank, LevelManager.Instance.startPoint.position, Quaternion.identity); 
+        GameObject Tank = Instantiate(tank, LevelManager.Instance.startPoint.position, Quaternion.identity);
+        if (isInfrared)
+        {
+          Renderer _Renderer = Tank.GetComponent<Renderer>();
+          _Renderer.material = new Material(infraredMaterial);
+        } 
     }
 
     public void SpawnTankRinged()
     {
         GameObject Tankringed = Instantiate(tankringed, LevelManager.Instance.startPoint.position, Quaternion.identity); 
+        if (isInfrared)
+        {
+          Renderer _Renderer = Tankringed.GetComponent<Renderer>();
+          _Renderer.material = new Material(infraredMaterial);
+        } 
     }
 
     public void SpawnSpeedster()
     {
-        GameObject Speedster = Instantiate(speedster, LevelManager.Instance.startPoint.position, Quaternion.identity); 
+        GameObject Speedster = Instantiate(speedster, LevelManager.Instance.startPoint.position, Quaternion.identity);         
+        if (isInfrared)
+        {
+          Renderer _Renderer = Speedster.GetComponent<Renderer>();
+          _Renderer.material = new Material(infraredMaterial);
+        } 
     }
 
     public void SpawnSpeedsterRinged()
     {
         GameObject Speedsterringed = Instantiate(speedsterringed, LevelManager.Instance.startPoint.position, Quaternion.identity); 
+        if (isInfrared)
+        {
+          Renderer _Renderer = Speedsterringed.GetComponent<Renderer>();
+          _Renderer.material = new Material(infraredMaterial);
+        } 
     }
 
 }
